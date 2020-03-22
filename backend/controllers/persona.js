08d6f2b16c9959c54eb411b0132d24816e2fa051
Daniel Portugal de Lourenço <daniportugal@gmail.com>
2020-03-22 18:54:49 -0300

    comentarios para entender personas

--- backend/controllers/persona.js
+++ backend/controllers/persona.js
@@ -1,11 +1,10 @@
 module.exports = {
     async createPersona(req, res) {
-        let name = req.params.nome;
-        let userId = req.params.idUsuario;
-        let species = req.params.especie;
+        let name = req.params.nome; //esse cara vai pro banco de dados, é o nome da persona
+        let userId = req.params.idUsuario; //para associar a persona ao user correto no banco
+        let species = req.params.especie; //pode ser "humano" ou "gulok" ou ""; se for "" é humano.
 
-        //res.send("personaOK " + name + " " + userId + " " + species);
-        //res.send("personaOK Loki 10 gulok");
-        res.send("personaOK newUser 99 humano");
+        //res.send("personaOK " + username + " " + personaId + " " + species);
+        res.send("personaOK newUser 99 humano"); //o nome da persona não é retornado ao client; está correto.
     }
 }
