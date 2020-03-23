416d278af27f1c7a4cbf56ef5176c94fe4985f61
Daniel Portugal de Lourenço <daniportugal@gmail.com>
2020-03-23 17:56:19 -0300

    todos

--- backend/torque/index.js
+++ backend/torque/index.js
@@ -12,3 +12,64 @@ api.use('/persona', persona)
 api.use('/academia', academia)
 
 module.exports = api
+
+//TODO
+/*
+- Criar sala:
+/sala/adicionar ? idPersona=XX & sala='sala 1' & login=username
+Resposta? ACHO que é:
+idJogo 12
+
+- Entrar em uma sala:
+/sala/adicionar ? idPersona=XX & idJogo=12 & login=username
+Resposta? ACHO que é:
+idJogo 12
+
+- Sair de uma sala:
+/sala/remover ? idPersona=XX & idJogo=12 & login=username
+Resposta? ACHO que é:
+idJogo 12
+
+- Finalizar um jogo:
+/jogo/finalizar?
+idJogo=12
+&
+idJogoTorque=1
+&
+duracao=16789
+&
+idsPersona=1;6;8;
+&
+creditos=10;6;1;
+&
+b_vitoria=1;0;0;
+&
+pontos=8;7;6;
+&
+b_visionario=1;0;0;
+&
+b_atacou=0;1;0;
+&
+b_arrebatador=0;0;1;
+&
+b_comerciante=1;1;0;
+&
+onde_saiu=1;3;2; (é o ID interno do Grupo; China == 3, e assim por diante)
+&
+id_obj1=2;6;23;
+&
+id_obj2=3;7;24;
+&
+qts_matou=0;0;1;
+&
+b_pk_power_play=0;0;0;
+&
+termino=F;F;S;  //F = finalizou; S = suicidou; R = rendeu;
+&
+planeta=2
+
+Resposta?
+ACHO que é:
+"OK " + gameId + " " + nextGameId
+OK 12 13
+*/
